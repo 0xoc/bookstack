f564d5d641b30994ea373eadc4f9476ef4dd7381
Sina Parvizi <snparvizi75@gmail.com>
2019-06-03 20:56:07 +0430

    phone number added to user profile | post save signal on user removed

--- bookstack/settings.py
+++ bookstack/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'bookCore',
     'users',
+    'phonenumber_field',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- users/models.py
+++ users/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 from django.dispatch import receiver
 from django.db.models import signals
+from phonenumber_field.modelfields import PhoneNumberField
+
 # Create your models here.
 
 
@@ -19,15 +21,11 @@ class UserProfile(models.Model):
                                help_text=_("Users's Avatar"),
                                blank=True, null=True)
 
+    phone_number = PhoneNumberField()
+
+    class Meta:
+        verbose_name = _('User Profile')
+        verbose_name_plural = _('Users Profiles')
+
     def __str__(self):
         return str(self.user)
-
-
-@receiver(signals.post_save, sender=User)
-def create_profile_for_user(sender, created, instance, **kwargs):
-    """
-    create a profile for every newly created django user
-    """
-
-    if created:
-        UserProfile.objects.create(user=instance)
